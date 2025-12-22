681c0285a2109313cc3e63025cb48b50fff8521b
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-12-21 22:36:07 -0600

    feat: add reusable Button component and integrate chadcn and Tailwind CSS for styling

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <Button>Hola mundo</Button>
+    </div>
+  )
 }

f6b9decb8e7713f7edd731763247b0048db7b919
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-12-22 18:01:43 -0600

    feat: add UI components for card, checkbox, label, radio group, and separator; implement filter sidebar and product card for shop page

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,12 +1,16 @@
 import { CustomPagination } from '@/components/custom/CustomPagination'
 import { Button } from '@/components/ui/button'
+import { products } from '@/mocks/products.mock'
 import { CustomJumbotron } from '@/shop/components/CustomJumbotron'
+import { ProductsGrid } from '@/shop/components/ProductsGrid'
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Teslo Shop" />
-      
+
+      <ProductsGrid products={products} />
+
       <CustomPagination totalPages={7} />
     </>
   )

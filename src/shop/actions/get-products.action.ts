0793e7dd65ea707627ee50f9a134c3846298ca00
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-12-28 15:27:22 -0600

    feat: implement product filtering and pagination; enhance getProductsAction and useProducts hook

--- src/shop/actions/get-products.action.ts
+++ src/shop/actions/get-products.action.ts
@@ -3,8 +3,30 @@ import type { ProductsResponse } from '@/interfaces/products.response'
 
 const API_URL = import.meta.env.VITE_API_URL
 
-export const getProductsAction = async (): Promise<ProductsResponse> => {
-  const { data } = await tesloApi.get<ProductsResponse>('/products')
+interface Options {
+  limit?: number | string
+  offset?: number | string
+  gender?: string
+  sizes?: string
+  minPrice?: number
+  maxPrice?: number
+}
+
+export const getProductsAction = async (
+  options: Options
+): Promise<ProductsResponse> => {
+  const { limit, offset, gender, sizes, minPrice, maxPrice } = options
+
+  const { data } = await tesloApi.get<ProductsResponse>('/products', {
+    params: {
+      limit,
+      offset,
+      gender,
+      sizes,
+      minPrice,
+      maxPrice,
+    },
+  })
 
   const productsWithUrls = data.products.map((product) => {
     const newImages = product.images.map((image) => {
--- src/shop/hooks/useProducts.tsx
+++ src/shop/hooks/useProducts.tsx
@@ -1,11 +1,57 @@
 import { useQuery } from '@tanstack/react-query'
 import { getProductsAction } from '../actions/get-products.action'
+import { useParams, useSearchParams } from 'react-router'
 
 export const useProducts = () => {
   // TODO: Add query logic
+  const [searchParams] = useSearchParams()
+  const { gender } = useParams()
+
+  const limit = searchParams.get('limit') || 9
+  const page = searchParams.get('page') || 1
+  const offset = (+page - 1) * +limit
+  const sizes = searchParams.get('sizes') || undefined
+  const price = searchParams.get('price') || 'any'
+
+  let minPrice = undefined
+  let maxPrice = undefined
+
+  switch (price) {
+    case '0-50': {
+      minPrice = 0
+      maxPrice = 50
+      break
+    }
+
+    case '50-100': {
+      minPrice = 50
+      maxPrice = 100
+      break
+    }
+
+    case '100-200': {
+      minPrice = 100
+      maxPrice = 200
+      break
+    }
+
+    case '200+': {
+      minPrice = 200
+      break
+    }
+  }
 
   return useQuery({
-    queryKey: ['products'],
-    queryFn: getProductsAction,
+    queryKey: ['products', { limit, page, gender, sizes, minPrice, maxPrice }],
+    queryFn: () =>
+      getProductsAction({
+        limit: isNaN(+limit) ? 9 : limit,
+        offset: isNaN(offset) ? 0 : offset,
+        gender,
+        sizes,
+        minPrice,
+        maxPrice,
+      }),
+    staleTime: 1000 * 60 * 5,
   })
 }

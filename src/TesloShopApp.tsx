7eb0e47ec98a7bf8722a69dc2c84c4a7a2fb0e10
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-12-29 13:55:08 -0600

    feat: add sonner for toast notifications

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -3,14 +3,16 @@ import { appRouter } from './app.router'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'sonner'
 
 const queryClient = new QueryClient()
 
 export const TesloShopApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={appRouter} />
+      <Toaster />
 
+      <RouterProvider router={appRouter} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )

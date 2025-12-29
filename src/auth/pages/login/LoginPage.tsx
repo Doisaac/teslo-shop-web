88d6171dca6b23f2698ec95d186a0ba35ef079d0
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-12-29 15:33:29 -0600

    feat: add zustand as a dependency for state management; add initial login state logic

--- src/auth/pages/login/LoginPage.tsx
+++ src/auth/pages/login/LoginPage.tsx
@@ -1,16 +1,45 @@
+import { Link, useNavigate } from 'react-router'
+import { useState, type FormEvent } from 'react'
+import { toast } from 'sonner'
+
+
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { CustomLogo } from '@/components/custom/CustomLogo'
-import { Link } from 'react-router'
+import { useAuthStore } from '@/auth/store/auth.store'
 
 export const LoginPage = () => {
+  const navigate = useNavigate()
+  const { login } = useAuthStore()
+
+  const [isPosting, setIsPosting] = useState(false)
+
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setIsPosting(true)
+
+    const formData = new FormData(event.target as HTMLFormElement)
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+
+    const isLoginValid = await login(email, password)
+
+    if (isLoginValid) {
+      navigate('/')
+      return
+    }
+
+    toast.error('Correo o/y contraseña no validos')
+    setIsPosting(false)
+  }
+
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={(e) => handleLogin(e)}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <CustomLogo />
@@ -22,6 +51,7 @@ export const LoginPage = () => {
                 <Label htmlFor="email">Correo</Label>
                 <Input
                   id="email"
+                  name="email"
                   type="email"
                   placeholder="mail@gmail.com"
                   required
@@ -39,12 +69,13 @@ export const LoginPage = () => {
                 </div>
                 <Input
                   id="password"
+                  name="password"
                   type="password"
                   required
                   placeholder="Contraseña"
                 />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isPosting}>
                 Ingresar
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
